d33789f25aec2118e4a371159c1414e96231d9f2
binit92 <kumar.binit1992@gmail.com>
2016-08-23 05:08:54 +0530

    Windows: Deleting x64 and update in installer.bat

--- install_script/installer.bat
+++ install_script/installer.bat
@@ -1,4 +1,3 @@
-
 @echo off
 
 :: --------------------------------
@@ -39,6 +38,8 @@ if defined ProgramFiles(x86) (
     @echo Some 32-bit work
     SET "flag=32"
     SET	CURL=%DIR%curl\x86
+	ECHO "32-bit is not certified yet!"
+	goto :endProcess
 )
 ECHO OS Architecture %FLAG%
 ECHO CURL path is: %CURL%
@@ -47,19 +48,53 @@ SET PATH=%PATH%;%CURL%
 
 
 :: -------------------------------------------------------------------------
-:: Download and Install 7Zip (To extract other software during installation) only installing x86 version
+:: Use 7Zip, (To extract other software during installation)
 :: -------------------------------------------------------------------------
 	:7zip
 	IF EXIST C:\Program Files\7-zip\ (goto :7ZInstalled)	
 	:: if 7zip is installed already, don't download it.
 	
 	ECHO If not installed, use 7za
-	SET 7ZA=%DIR%7za
-	SET PATH=%PATH%;%7ZA%
-	goto MinGw
+	ECHO %DIR%7za
+	
+	ECHO "Setting Path for 7za"
+	SET PATH=%PATH%;%DIR%7za
+	goto ANACONDA
 
 	:7ZInstalled
 	ECHO 7Z is installed already 
+	
+:: -------------------------------------
+:: Download and install Anaconda 
+:: -------------------------------------	
+	:ANACONDA
+	
+	IF EXIST C:\Anaconda (
+	SET ANACONDA=C:\Anaconda
+	SET ANACONDASCRIPTS=C:\Anaconda\Scripts
+	SET PATH=%PATH%;%ANACONDA%;%ANACONDASCRIPTS%
+	goto anacondaInstalled
+	)
+		
+	IF EXIST %UserProfile%\Anaconda (
+	SET ANACONDA=%UserProfile%\Anaconda
+	SET ANACONDASCRIPTS=%UserProfile%Anaconda\Scripts
+	SET PATH=%PATH%;%ANACONDA%;%ANACONDASCRIPTS%
+	goto anacondaInstalled
+	)
+	
+	ECHO Downloading Anaconda-2.1.0-Windows-x86_64
+	curl  -O https://repo.continuum.io/archive/Anaconda-2.1.0-Windows-x86_64.exe
+	
+	ECHO Installing Anaconda and registering as System's Python 
+	Anaconda-2.1.0-Windows-x86_64.exe /RegisterPython=1
+	goto MinGw
+	
+	:anacondaInstalled
+	ECHO "Anaconda is installed already"
+	SET PATH=%PATH%;%ANACONDA%;%ANACONDASCRIPTS%
+	
+	
 ::-----------------------------------
 :: Download and Extract MINGW 4.8.1
 ::-----------------------------------
@@ -70,7 +105,7 @@ SET PATH=%PATH%;%CURL%
 	ECHO Downloading MINGW 4.8.1
 	curl -O https://sourceforge.net/projects/mingwbuilds/files/host-windows/releases/4.8.1/64-bit/threads-posix/sjlj/x64-4.8.1-release-posix-sjlj-rev5.7z/download
 	
-	ECHO Extracting MINGW 
+	ECHO Extracting MINGW USING 7Z
 	7z x x64-4.8.1-release-posix-sjlj-rev5.7z -oC:\
 	
 	ECHO Setting Environment Variables
@@ -79,39 +114,14 @@ SET PATH=%PATH%;%CURL%
 
 	:MinGwInstalled
 	ECHO MinGw is installed already 
-	ECHO Setting Environment Variables
 	SET PATH=%PATH%;%MINGW_ROOT%bin;%MINGW_ROOT%lib
 
-:: ---------------------------------------------------------------------------
-:: Copy Blas and Lapack from install_script folder to bundled/simulations/libs
-:: ---------------------------------------------------------------------------
-	
-	ECHO Copying BlAS and LAPACK
-	
-	:: Is this step required ? 
-	IF %flag%==32 ( copy %DIR%\blas\win32\libblas.dll %ROOT%bundled\simulation\libs )
-	IF %flag%==32 (copy %DIR%\lapack\win32\liblapack.dll %ROOT%bundled\simulation\libs )
-	
-	:: copy to MinGw Folder
-	IF %flag%==32 copy %DIR%\blas\win32\libblas.dll %MINGW_ROOT%lib
-	IF %flag%==32 copy %DIR%\lapack\win32\liblapack.dll %MINGW_ROOT%lib
-
-	:: Is this step required ? 
-	IF %flag%==64 copy %DIR%\blas\x64\libblas.dll %ROOT%bundled\simulation\libs
-	IF %flag%==64 copy %DIR%\lapack\x64\liblapack.dll %ROOT%bundled\simulation\libs
-	
-	:: copy to MinGw Folder 
-	IF %flag%==64 copy %DIR%\blas\x64\libblas.dll %MINGW_ROOT%lib
-	IF %flag%==64 copy %DIR%\lapack\x64\liblapack.dll %MINGW_ROOT%lib
 :: -----------------------------------------------
 :: Download and Install CMAKE
 :: -----------------------------------------------
-	:cmake
-	::cmake --version 2>NUL
-	::if errorlevel 0 goto CMAKEInstalled
-	
-	SET CMAKE=C:\cmake-2.8.12.2-win32-x86\bin
-	IF EXIST C:\cmake-2.8.12.2-win32-x86\ (goto CMAKEInstalled)
+		
+	SET CMAKE=C:\CMake\bin
+	IF EXIST C:\CMake\bin (goto CMAKEInstalled)
 
 	ECHO Downloading CMAKE 2.8
 	curl -O https://cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip
@@ -127,95 +137,86 @@ SET PATH=%PATH%;%CURL%
 	:CMAKEInstalled
 	ECHO CMake is installed already
 	SET PATH=%PATH%;%CMAKE%
-	ECHO PATH IS %PATH%
+	ECHO PATH IS: %PATH%	
 
 :: ----------------------------------
-:: Making GFortran Build Folder
+:: Making GFortran Build Folder (create an empty folder to create makefiles ! )
 :: ----------------------------------
 	:gfortranBuild
 	CD %PARENTDIR%
 	ECHO Creating new GFortran build folder 
-	RMDIR source\NemohImproved\Nemoh\gFortranBuildWindows /S /Q
-	MKDIR source\NemohImproved\Nemoh\gFortranBuildWindows
-	CD %PARENTDIR%source\NemohImproved\Nemoh\gFortranBuildWindows
-	::CD source
+	RMDIR source\NemohImproved\Nemoh\gFortranBuild /S /Q
+	MKDIR source\NemohImproved\Nemoh\gFortranBuild
+	CD %PARENTDIR%source\NemohImproved\Nemoh\
 	goto build
-	:: delete this folder on new installation
+		
+
 :: -----------------------------------------------
 :: Build libnemoh.dll and libnemoh.dll.a 
 :: -----------------------------------------------
 	:build
 	cmake -DCMAKE_Fortran_COMPILER="gfortran" "%PARENTDIR%source\NemohImproved\Nemoh" -G "MinGW Makefiles"
-	::cmake -DCMAKE_Fortran_COMPILER="gfortran" "%PARENTDIR%source/NemohImproved/Nemoh/" -G "MinGW MakeFiles"
-	::cmake -DCMAKE_Fortran_COMPILER="gfortran" "%PARENTDIR%source/NemohImproved/Nemoh/" 
 	mingw32-make
-	
-	CD %DIR%
-:: ------------------------------------------------------------------------
-:: Copy libnemoh.dll and libnemoh.dll.a from simulation\libs to MinGw\lib
-:: -----------------------------------------------------------------------
-	ECHO "Copying libnemoh.dll"
-	copy %ROOT%bundled\simulation\libs\libnemoh.dll %MINGW_ROOT%lib
-	copy %ROOT%bundled\simulation\libs\libnemoh.dll %MINGW_ROOT%lib
-	copy %ROOT%bundled\simulation\libs\libnemoh.dll %MINGW_ROOT%lib
-	copy %ROOT%bundled\simulation\libs\libnemoh.dll %MINGW_ROOT%lib
+	CD %PARENTDIR%
 
-:: Download and Install Anaconda 2
-:: ----------------------------------------------------
+:: --------------------------------------------------------------------
+:: Copy libnemoh.dll to Anaconda/Dlls, Anaconda/Libs and MinGW/libs	
+:: --------------------------------------------------------------------
+	ECHO Copying libnemoh.dll to Anaconda\Dlls
+	copy %PARENTDIR%source\NemohImproved\Nemoh\libnemoh.dll %ANACONDA%DLLs
+	copy %PARENTDIR%source\NemohImproved\Nemoh\libnemoh.dll.a %ANACONDA%DLLs
 	
-	ECHO Downloading Anaconda
-	SET ANACONDA=C:\Anaconda2
-	IF EXIST C:\Anaconda2 goto :AnacondaInstalled
+	ECHO Copying libnemoh.dll to Anaconda\libs
+	copy %PARENTDIR%source\NemohImproved\Nemoh\libnemoh.dll %ANACONDA%libs
+	copy %PARENTDIR%source\NemohImproved\Nemoh\ibnemoh.dll.a %ANACONDA%libs
 	
-	::curl -O https://repo.continuum.io/archive/.winzip/Anaconda2-4.1.1-Windows-x86_64.zip
-	ECHO Extracting Anaconda
-	7z x %DIR%Anaconda2-4.1.1-Windows-x86_64.zip -oC:\
+	ECHO Copying libnemoh.dll to Anaconda
+	copy %PARENTDIR%source\NemohImproved\Nemoh\libnemoh.dll %MINGW_ROOT%lib
+	copy %PARENTDIR%source\NemohImproved\Nemoh\libnemoh.dll.a %MINGW_ROOT%lib
 	
-	ECHO Installing Anaconda
-	Anaconda2-4.1.1-Windows-x86_64.exe
 	
-	ECHO Setting Env for Anaconda
-	SET ANACONDA=C:\Anaconda2
+:: ---------------------------------------------------------------------------
+:: Copy Blas and Lapack from install_script folder to bundled/simulations/libs
+:: ---------------------------------------------------------------------------
+	
+	ECHO Copying BlAS and LAPACK to MinGW/libs
+	copy %PARENTDIR%install_script\dlls\libblas.dll %MINGW_ROOT%lib% 
+	copy %PARENTDIR%install_script\dlls\liblapack.dll %MINGW_ROOT%lib% 
+	
+	ECHO Copying 19 necessary Dlls inside Anaconda\Dlls
+	xcopy /s %PARENTDIR%install_script\dlls %ANACONDA%DLLs
 
-	:AnacondaInstalled
-	ECHO "Anaconda Installed already "
-	SET PATH=%PATH%;%ANACONDA%;%ANACONDA%\Scripts
+
 :: ----------------------------------------
-:: Install CherryPy and other requirements
+:: Installing Python libraries using pip
 :: -----------------------------------------
 
-	ECHO Instally CherryPy and other requirements
-	
+	ECHO Installing required python libraries using pip
+	ECHO %ROOT%
+	ECHO %ANACONDA%\Scripts\pip
+		
 	%ANACONDA%\Scripts\pip install -r %ROOT%requirements.txt --upgrade
 
-	:: Remove this later on, I was getting vrvasall.bat missing 
-	:: pip install –upgrade setuptools "<-- this command is not working "
-	:: pip install setuptools --no-use-wheel --upgrade (<-- not helping )
-
-	:: psycopyg windows (need to have Git installed for it ! )
-	::pip install git+https://github.com/nwcell/psycopg2-windows.git@win64-py27#egg=psycopg2
+	
 :: --------------------------------
-:: Install ParaView
+:: Installing ParaView
 :: ---------------------------------
+	ECHO Downloading Paraview 
+	curl -L http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.1&type=binary&os=win64&downloadFile=ParaView-4.1.0-Windows-64bit.exe
 
-
-
+	ECHO Installing Paraview
+	ParaView-4.1.0-Windows-64bit.exe
+	
+	
 :: --------------------------------
-:: Build Setup.py
+:: Building Solver Fortran 
 :: -------------------------------
 
 	ECHO Building setup.py 
 	python %ROOT%\nemoh\setup.py cleanall
 	python %ROOT%\nemoh\setup.py build_ext --inplace
 
-:: -------------------------------
-:: Set Path variable
-:: -----------------------------
-
-:: Finally SET the path so that it can be used later !
-::SETX PATH "%PATH%;%ANACONDA%;%ANACONDA%\Scripts /m
-
-ECHO "OpenWarp Installation Steps are complete, Check the logs for possible errors ! "
+	ECHO "OpenWarp Installation Steps are complete"
 
 
 :: ------------------------------
@@ -224,3 +225,6 @@ ECHO "OpenWarp Installation Steps are complete, Check the logs for possible erro
 	ECHO "RUNNING OPENWARP"
 	CD %ROOT%
 	python main.py
+	
+:endProcess
+ECHO "Ending Process !"	
