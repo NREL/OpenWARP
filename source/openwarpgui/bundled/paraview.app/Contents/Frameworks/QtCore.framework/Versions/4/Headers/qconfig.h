0b79c110f8d72b3ccb18a0ae0c1a6f1662f16e5c
binit92 <binit92>
2016-09-17 13:59:13 +0000

    Mac: Adds paraview.app inside bundled, it will add 300mb extra in the repo

---
+++ source/openwarpgui/bundled/paraview.app/Contents/Frameworks/QtCore.framework/Versions/4/Headers/qconfig.h
@@ -0,0 +1,248 @@
+/* Everything */
+
+/* Qt Edition */
+#ifndef QT_EDITION
+#  define QT_EDITION QT_EDITION_OPENSOURCE
+#endif
+
+/* Machine byte-order */
+#define Q_BIG_ENDIAN 4321
+#define Q_LITTLE_ENDIAN 1234
+#define QT_BUILD_KEY "macosx macx-cocoa clang no-pkg-config"
+#define QT_BUILD_KEY_COMPAT "i386 Darwin clang no-pkg-config"
+
+#ifdef QT_BOOTSTRAPPED
+#if defined(__BIG_ENDIAN__)
+# define Q_BYTE_ORDER Q_BIG_ENDIAN
+#elif defined(__LITTLE_ENDIAN__)
+# define Q_BYTE_ORDER Q_LITTLE_ENDIAN
+#else
+# error "Unable to determine byte order!"
+#endif
+#else
+#if defined(__BIG_ENDIAN__)
+# define Q_BYTE_ORDER Q_BIG_ENDIAN
+#elif defined(__LITTLE_ENDIAN__)
+# define Q_BYTE_ORDER Q_LITTLE_ENDIAN
+#else
+# error "Unable to determine byte order!"
+#endif
+#endif
+/* Machine Architecture */
+#ifndef QT_BOOTSTRAPPED
+# define QT_ARCH_MACOSX
+#else
+# define QT_ARCH_MACOSX
+#endif
+/* Compile time features */
+#define QT_LARGEFILE_SUPPORT 64
+#define QT_MAC_USE_COCOA 1
+#define QT_MAC_FRAMEWORK_BUILD
+#if defined(__LP64__)
+# define QT_POINTER_SIZE 8
+#else
+# define QT_POINTER_SIZE 4
+#endif
+
+#ifndef QT_BOOTSTRAPPED
+
+#ifndef QDECLARATIVE_NO_DEBUG_PROTOCOL
+# define QDECLARATIVE_NO_DEBUG_PROTOCOL
+#endif
+
+#if defined(QT_NO_ALSA) && defined(QT_ALSA)
+# undef QT_NO_ALSA
+#elif !defined(QT_NO_ALSA) && !defined(QT_ALSA)
+# define QT_NO_ALSA
+#endif
+
+#if defined(QT_NO_CLOCK_MONOTONIC) && defined(QT_CLOCK_MONOTONIC)
+# undef QT_NO_CLOCK_MONOTONIC
+#elif !defined(QT_NO_CLOCK_MONOTONIC) && !defined(QT_CLOCK_MONOTONIC)
+# define QT_NO_CLOCK_MONOTONIC
+#endif
+
+#if defined(QT_NO_DBUS) && defined(QT_DBUS)
+# undef QT_NO_DBUS
+#elif !defined(QT_NO_DBUS) && !defined(QT_DBUS)
+# define QT_NO_DBUS
+#endif
+
+#if defined(QT_NO_EGL) && defined(QT_EGL)
+# undef QT_NO_EGL
+#elif !defined(QT_NO_EGL) && !defined(QT_EGL)
+# define QT_NO_EGL
+#endif
+
+#if defined(QT_NO_FREETYPE) && defined(QT_FREETYPE)
+# undef QT_NO_FREETYPE
+#elif !defined(QT_NO_FREETYPE) && !defined(QT_FREETYPE)
+# define QT_NO_FREETYPE
+#endif
+
+#if defined(QT_NO_GLIB) && defined(QT_GLIB)
+# undef QT_NO_GLIB
+#elif !defined(QT_NO_GLIB) && !defined(QT_GLIB)
+# define QT_NO_GLIB
+#endif
+
+#if defined(QT_NO_GSTREAMER) && defined(QT_GSTREAMER)
+# undef QT_NO_GSTREAMER
+#elif !defined(QT_NO_GSTREAMER) && !defined(QT_GSTREAMER)
+# define QT_NO_GSTREAMER
+#endif
+
+#if defined(QT_NO_ICD) && defined(QT_ICD)
+# undef QT_NO_ICD
+#elif !defined(QT_NO_ICD) && !defined(QT_ICD)
+# define QT_NO_ICD
+#endif
+
+#if defined(QT_NO_IMAGEFORMAT_JPEG) && defined(QT_IMAGEFORMAT_JPEG)
+# undef QT_NO_IMAGEFORMAT_JPEG
+#elif !defined(QT_NO_IMAGEFORMAT_JPEG) && !defined(QT_IMAGEFORMAT_JPEG)
+# define QT_NO_IMAGEFORMAT_JPEG
+#endif
+
+#if defined(QT_NO_IMAGEFORMAT_MNG) && defined(QT_IMAGEFORMAT_MNG)
+# undef QT_NO_IMAGEFORMAT_MNG
+#elif !defined(QT_NO_IMAGEFORMAT_MNG) && !defined(QT_IMAGEFORMAT_MNG)
+# define QT_NO_IMAGEFORMAT_MNG
+#endif
+
+#if defined(QT_NO_IMAGEFORMAT_TIFF) && defined(QT_IMAGEFORMAT_TIFF)
+# undef QT_NO_IMAGEFORMAT_TIFF
+#elif !defined(QT_NO_IMAGEFORMAT_TIFF) && !defined(QT_IMAGEFORMAT_TIFF)
+# define QT_NO_IMAGEFORMAT_TIFF
+#endif
+
+#if defined(QT_NO_INOTIFY) && defined(QT_INOTIFY)
+# undef QT_NO_INOTIFY
+#elif !defined(QT_NO_INOTIFY) && !defined(QT_INOTIFY)
+# define QT_NO_INOTIFY
+#endif
+
+#if defined(QT_NO_MREMAP) && defined(QT_MREMAP)
+# undef QT_NO_MREMAP
+#elif !defined(QT_NO_MREMAP) && !defined(QT_MREMAP)
+# define QT_NO_MREMAP
+#endif
+
+#if defined(QT_NO_MULTIMEDIA) && defined(QT_MULTIMEDIA)
+# undef QT_NO_MULTIMEDIA
+#elif !defined(QT_NO_MULTIMEDIA) && !defined(QT_MULTIMEDIA)
+# define QT_NO_MULTIMEDIA
+#endif
+
+#if defined(QT_NO_NAS) && defined(QT_NAS)
+# undef QT_NO_NAS
+#elif !defined(QT_NO_NAS) && !defined(QT_NAS)
+# define QT_NO_NAS
+#endif
+
+#if defined(QT_NO_OPENSSL) && defined(QT_OPENSSL)
+# undef QT_NO_OPENSSL
+#elif !defined(QT_NO_OPENSSL) && !defined(QT_OPENSSL)
+# define QT_NO_OPENSSL
+#endif
+
+#if defined(QT_NO_OPENVG) && defined(QT_OPENVG)
+# undef QT_NO_OPENVG
+#elif !defined(QT_NO_OPENVG) && !defined(QT_OPENVG)
+# define QT_NO_OPENVG
+#endif
+
+#if defined(QT_NO_PHONON) && defined(QT_PHONON)
+# undef QT_NO_PHONON
+#elif !defined(QT_NO_PHONON) && !defined(QT_PHONON)
+# define QT_NO_PHONON
+#endif
+
+#if defined(QT_NO_S60) && defined(QT_S60)
+# undef QT_NO_S60
+#elif !defined(QT_NO_S60) && !defined(QT_S60)
+# define QT_NO_S60
+#endif
+
+#if defined(QT_NO_SCRIPTTOOLS) && defined(QT_SCRIPTTOOLS)
+# undef QT_NO_SCRIPTTOOLS
+#elif !defined(QT_NO_SCRIPTTOOLS) && !defined(QT_SCRIPTTOOLS)
+# define QT_NO_SCRIPTTOOLS
+#endif
+
+#if defined(QT_NO_STYLE_GTK) && defined(QT_STYLE_GTK)
+# undef QT_NO_STYLE_GTK
+#elif !defined(QT_NO_STYLE_GTK) && !defined(QT_STYLE_GTK)
+# define QT_NO_STYLE_GTK
+#endif
+
+#if defined(QT_NO_STYLE_S60) && defined(QT_STYLE_S60)
+# undef QT_NO_STYLE_S60
+#elif !defined(QT_NO_STYLE_S60) && !defined(QT_STYLE_S60)
+# define QT_NO_STYLE_S60
+#endif
+
+#if defined(QT_NO_SVG) && defined(QT_SVG)
+# undef QT_NO_SVG
+#elif !defined(QT_NO_SVG) && !defined(QT_SVG)
+# define QT_NO_SVG
+#endif
+
+#if defined(QT_NO_SXE) && defined(QT_SXE)
+# undef QT_NO_SXE
+#elif !defined(QT_NO_SXE) && !defined(QT_SXE)
+# define QT_NO_SXE
+#endif
+
+#if defined(QT_NO_WEBKIT) && defined(QT_WEBKIT)
+# undef QT_NO_WEBKIT
+#elif !defined(QT_NO_WEBKIT) && !defined(QT_WEBKIT)
+# define QT_NO_WEBKIT
+#endif
+
+#if defined(QT_NO_XINERAMA) && defined(QT_XINERAMA)
+# undef QT_NO_XINERAMA
+#elif !defined(QT_NO_XINERAMA) && !defined(QT_XINERAMA)
+# define QT_NO_XINERAMA
+#endif
+
+#if defined(QT_NO_XVIDEO) && defined(QT_XVIDEO)
+# undef QT_NO_XVIDEO
+#elif !defined(QT_NO_XVIDEO) && !defined(QT_XVIDEO)
+# define QT_NO_XVIDEO
+#endif
+
+#if defined(QT_NO_ZLIB) && defined(QT_ZLIB)
+# undef QT_NO_ZLIB
+#elif !defined(QT_NO_ZLIB) && !defined(QT_ZLIB)
+# define QT_NO_ZLIB
+#endif
+
+#if defined(QT_RUNTIME_XCURSOR) && defined(QT_NO_RUNTIME_XCURSOR)
+# undef QT_RUNTIME_XCURSOR
+#elif !defined(QT_RUNTIME_XCURSOR) && !defined(QT_NO_RUNTIME_XCURSOR)
+# define QT_RUNTIME_XCURSOR
+#endif
+
+#if defined(QT_RUNTIME_XFIXES) && defined(QT_NO_RUNTIME_XFIXES)
+# undef QT_RUNTIME_XFIXES
+#elif !defined(QT_RUNTIME_XFIXES) && !defined(QT_NO_RUNTIME_XFIXES)
+# define QT_RUNTIME_XFIXES
+#endif
+
+#if defined(QT_RUNTIME_XINPUT) && defined(QT_NO_RUNTIME_XINPUT)
+# undef QT_RUNTIME_XINPUT
+#elif !defined(QT_RUNTIME_XINPUT) && !defined(QT_NO_RUNTIME_XINPUT)
+# define QT_RUNTIME_XINPUT
+#endif
+
+#if defined(QT_RUNTIME_XRANDR) && defined(QT_NO_RUNTIME_XRANDR)
+# undef QT_RUNTIME_XRANDR
+#elif !defined(QT_RUNTIME_XRANDR) && !defined(QT_NO_RUNTIME_XRANDR)
+# define QT_RUNTIME_XRANDR
+#endif
+
+#endif // QT_BOOTSTRAPPED
+
+#define QT_VISIBILITY_AVAILABLE
+
---
+++ source/openwarpgui/bundled/paraview.app/Contents/Python/paraview/vtk/vtkPVAnimation.py
@@ -0,0 +1,1 @@
+from vtkPVAnimationPython import *
